396e8013dee6efe70670739a3198dc4dddadafb8
BenMiriello <benmiriello@gmail.com>
2020-04-22 11:06:37 -0400

    initial solution: subarray-sum-equals-k

---
+++ subarray-sum-equals-k.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+var subarraySum = function(nums, k) {
+    let combos = 0;
+    
+    let sum = new Array(nums.length + 1);
+    sum[0] = 0;
+    
+    for (let i = 1; i <= nums.length; i++) {
+        sum[i] = sum[i - 1] + nums[i - 1];
+    };
+    
+    for (let start = 0; start < nums.length; start++) {
+        for (let end = start + 1; end <= nums.length; end++) {
+            if (sum[end] - sum[start] === k) {
+                combos++;
+            }
+        }
+    }
+
+    return combos;
+};
