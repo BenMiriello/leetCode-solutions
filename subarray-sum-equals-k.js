cca562772bb69ce8804f9f3dde1755fd6763d483
BenMiriello <benmiriello@gmail.com>
2020-04-22 11:10:06 -0400

    improved solution: subarray sum equals k

--- subarray-sum-equals-k.js
+++ subarray-sum-equals-k.js
@@ -5,22 +5,39 @@
  */
 
 var subarraySum = function(nums, k) {
-    let combos = 0;
-    
-    let sum = new Array(nums.length + 1);
-    sum[0] = 0;
-    
-    for (let i = 1; i <= nums.length; i++) {
-        sum[i] = sum[i - 1] + nums[i - 1];
-    };
+    let count = 0;
     
     for (let start = 0; start < nums.length; start++) {
-        for (let end = start + 1; end <= nums.length; end++) {
-            if (sum[end] - sum[start] === k) {
-                combos++;
+        let sum = 0;
+        for (let end = start; end < nums.length; end++) {
+            sum += nums[end];
+            if (sum === k) {
+                count++;
             }
         }
-    }
-
-    return combos;
+    };
+    
+    return count;
 };
+
+
+// var subarraySum = function(nums, k) {
+//     let combos = 0;
+    
+//     let sum = new Array(nums.length + 1);
+//     sum[0] = 0;
+    
+//     for (let i = 1; i <= nums.length; i++) {
+//         sum[i] = sum[i - 1] + nums[i - 1];
+//     };
+    
+//     for (let start = 0; start < nums.length; start++) {
+//         for (let end = start + 1; end <= nums.length; end++) {
+//             if (sum[end] - sum[start] === k) {
+//                 combos++;
+//             }
+//         }
+//     }
+
+//     return combos;
+// };
