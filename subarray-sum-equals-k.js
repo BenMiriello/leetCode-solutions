4dafeb770c2fc701645391f7275540181e9dfa90
BenMiriello <benmiriello@gmail.com>
2020-04-22 11:19:56 -0400

    3x faster solution: subarray sum equals k

--- subarray-sum-equals-k.js
+++ subarray-sum-equals-k.js
@@ -5,20 +5,31 @@
  */
 
 var subarraySum = function(nums, k) {
-    let count = 0;
-    
-    for (let start = 0; start < nums.length; start++) {
-        let sum = 0;
-        for (let end = start; end < nums.length; end++) {
-            sum += nums[end];
-            if (sum === k) {
-                count++;
-            }
-        }
-    };
-    
+    let count = sum = 0;
+    const map = {0: 1};
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (map[sum - k]) count += map[sum - k];
+        map[sum] = map[sum] ? map[sum] + 1 : 1
+    }
     return count;
 };
+
+// var subarraySum = function(nums, k) {
+//     let count = 0;
+    
+//     for (let start = 0; start < nums.length; start++) {
+//         let sum = 0;
+//         for (let end = start; end < nums.length; end++) {
+//             sum += nums[end];
+//             if (sum === k) {
+//                 count++;
+//             }
+//         }
+//     };
+    
+//     return count;
+// };
 
 
 // var subarraySum = function(nums, k) {
