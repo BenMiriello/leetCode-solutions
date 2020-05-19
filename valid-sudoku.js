5c2179218849ee504cd9d3c48c1cf02cfa1a55ba
BenMiriello <benmiriello@gmail.com>
2020-05-19 14:34:34 -0400

    rotate-image: incomplete solution

--- valid-sudoku.js
+++ valid-sudoku.js
@@ -7,7 +7,7 @@ var isValidSudoku = function(board) {
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             if ( board[r][c] != ".") {
-                if ((subBoxes[Math.floor(r / 3)][Math.floor(c / 3)] += parseInt(board[r][c]) > 9)) {
+                if ((subBoxes[Math.floor(r / 3)][Math.floor(c / 3)] += parseInt(board[r][c])) > 9) {
                     return false;
                 }
             }
