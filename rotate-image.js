40158333c916fa92241852d5f4071faa2c488ba9
BenMiriello <benmiriello@gmail.com>
2020-06-08 17:23:11 -0400

    rotate-image: fix tests

--- rotate-image.js
+++ rotate-image.js
@@ -46,5 +46,47 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 const rotate = matrix => {
-    
+    return matrix
 };
+
+const test = (matrix, expected) => {
+    console.log('\nTesting: ' + JSON.stringify(matrix))
+    const result = rotate(JSON.stringify(matrix))
+    if (result === expected) console.log('PASSED ' + result)
+    else {
+        console.log('FAILED ' + result)
+        console.log('Expected: ', expected)
+    }
+}
+
+let matrix, expected
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+expected = [
+    [7,4,1],
+    [8,5,2],
+    [9,6,3]
+]
+
+test(matrix, expected)
+
+matrix = [
+    [ 5, 1, 9,11],
+    [ 2, 4, 8,10],
+    [13, 3, 6, 7],
+    [15,14,12,16]
+]
+
+expected = [
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+
+test(matrix, expected)
