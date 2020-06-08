08bf71ee6d0996e587c1085eb7357f921c14f5e5
BenMiriello <benmiriello@gmail.com>
2020-06-08 10:17:03 -0400

    much better solution: maximum-subaray

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -18,12 +18,28 @@ If you have figured out the O(n) solution, try coding another solution using the
 
 // SOLUTION
 
-var maxSubArray = function(nums) {
-    for (let n = 1; n < nums.length; n++){
-      nums[n] = Math.max(nums[n], nums[n] + nums[n-1])
-    }
-    return Math.max(...nums)
+function maxSubArray(nums) {
+  for (let n = 1; n < nums.length; n++){
+    nums[n] = Math.max(nums[n], nums[n] + nums[n-1])
+  }
+  return Math.max(...nums)
 };
+
+// function maxSubArray(array){
+//   if (array.length < 1) return null;
+//   if (array.length === 1) return array[0];
+//   let max = -Number.MAX_VALUE;
+//   for (let start = 0; start < array.length; start++) {
+//     for (let end = start; end <= array.length; end++) {
+//       let subtotal = 0;
+//       for (let i = start; i < end; i++) {
+//         subtotal += array[i];
+//       }
+//       if (subtotal > max) max = subtotal;
+//     }
+//   }
+//   return max;
+// };
 
 let test = (nums, desired) => {
   let result = maxSubArray(nums)
