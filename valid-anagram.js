391b32b6713f6f4f396a37553469d210e27d52a0
BenMiriello <benmiriello@gmail.com>
2020-06-12 10:57:33 -0400

    valid anagram hash map middle runtime solution

--- valid-anagram.js
+++ valid-anagram.js
@@ -4,4 +4,23 @@
  * @return {boolean}
  */
 
-const isAnagram = (s, t) => s.split('').sort().join('') === t.split('').sort().join('');
+// const isAnagram = (s, t) => s.split('').sort().join('') === t.split('').sort().join('');
+const isAnagram = (s, t) => {
+    if (s.length != t.length) {
+        return false
+    };
+
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] ? map[s[i]]++ : map[s[i]] = 1;
+        map[t[i]] ? map[t[i]]-- : map[t[i]] = -1;
+    }
+    
+    for (let char in map) {
+        if (map[char] != 0) {
+            return false
+        };
+    }
+    
+    return true;
+}
