de483e228a17e9ba26ebb0b61e72c4c967e36542
BenMiriello <benmiriello@gmail.com>
2020-06-22 20:51:39 -0400

    Solution (refactored): two-sum

--- two-sum.js
+++ two-sum.js
@@ -3,31 +3,43 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
     const map = {};
     for (let i = 0; i < nums.length; i++) {
         map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
     };
     for (let i = 0; i < nums.length; i++) {
         if (map[target - nums[i]]) {
-            if (nums[i] * 2 === target) {
-                if (map[nums[i]] >= 2) {
-                    let x = nums[i]
-                    nums[i] = Number.MAX_VALUE - 3.7295
-                    return [i, nums.indexOf(x)];
-                } else continue
-            } else {
-                return [i, nums.indexOf(target - nums[i])];  
-            }
+            if (nums[i] * 2 === target && !map[nums[i]] >= 2) continue
+            let x = nums[i]
+            nums[i] = Number.MAX_VALUE - 3.7295
+            return [i, nums.indexOf( nums[i] * 2 === target ? x : target - nums[i])];
         }
     }
 };
 
-// /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number[]}
-//  */
+
+// var twoSum = function(nums, target) {
+//     const map = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
+//     };
+//     for (let i = 0; i < nums.length; i++) {
+//         if (map[target - nums[i]]) {
+//             if (nums[i] * 2 === target) {
+//                 if (map[nums[i]] >= 2) {
+//                     let x = nums[i]
+//                     nums[i] = Number.MAX_VALUE - 3.7295
+//                     return [i, nums.indexOf(x)];
+//                 } else continue
+//             } else {
+//                 return [i, nums.indexOf(target - nums[i])];  
+//             }
+//         }
+//     }
+// };
+
+
 // var twoSum = function(nums, target) {
 //     for (let i = 0; i < nums.length; i++) {
 //         for (let j = i + 1; j < nums.length; j++) {
