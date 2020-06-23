b6719ec20537456d38d66b1ff2e2bf274c460bde
BenMiriello <benmiriello@gmail.com>
2020-06-22 20:43:16 -0400

    Solution (On): two-sum

--- two-sum.js
+++ two-sum.js
@@ -4,12 +4,37 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    const map = {};
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            console.log(i, j)
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
+        map[nums[i]] = map[nums[i]] ? map[nums[i]] + 1 : 1;
+    };
+    for (let i = 0; i < nums.length; i++) {
+        if (map[target - nums[i]]) {
+            if (nums[i] * 2 === target) {
+                if (map[nums[i]] >= 2) {
+                    let x = nums[i]
+                    nums[i] = Number.MAX_VALUE - 3.7295
+                    return [i, nums.indexOf(x)];
+                } else continue
+            } else {
+                return [i, nums.indexOf(target - nums[i])];  
             }
         }
-    };
+    }
 };
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             console.log(i, j)
+//             if (nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//     };
+// };
