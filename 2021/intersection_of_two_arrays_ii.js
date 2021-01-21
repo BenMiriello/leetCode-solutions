53a94b7a0287b71f8daf2729468150045e2e8d61
BenMiriello <benmiriello@gmail.com>
2021-01-20 23:43:57 -0500

    Add Intersection of two arrays base w/o solution

---
+++ 2021/intersection_of_two_arrays_ii.js
@@ -0,0 +1,20 @@
+const intersect = (nums1, nums2) => {
+  // return []
+}
+
+tests = [
+  {
+    input: [[1,2,2,1], [2,2]],
+    expected: [2,2]
+  },
+  {
+    input: [[4,9,5], [9,4,9,8,4]],
+    expected: [4,9]
+  }
+]
+
+module.exports = {
+  name: 'Intersection of two arrays II',
+  method: intersect,
+  tests
+};
