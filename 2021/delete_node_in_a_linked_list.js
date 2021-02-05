36403daedb912acc13783573546fc8f8ee32e678
BenMiriello <benmiriello@gmail.com>
2021-02-05 00:05:01 -0500

    Create delete node in a linked list problem file

---
+++ 2021/delete_node_in_a_linked_list.js
@@ -0,0 +1,22 @@
+const deleteNode = (node) => {};
+
+const tests = [
+  {
+    input: [],
+    expected: null
+  },
+  {
+    input: [],
+    expected: null
+  },
+  {
+    input: [],
+    expected: null
+  }
+]
+
+module.exports = {
+  name: 'delete node in a linked list',
+  method: deleteNode,
+  tests
+};
