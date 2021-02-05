8a1ef4f81e08af9481c7c907a8e2645a0bb5e20a
BenMiriello <benmiriello@gmail.com>
2021-02-05 00:02:14 -0500

    Setup new file generator to accept up to 4 arguments for function

--- newfile.js
+++ newfile.js
@@ -5,8 +5,15 @@ const { camelize } = require('./utilities');
 const filename = process.argv[2];
 const funcname = process.argv[3] || camelize(filename);
 
-write.sync(`2021/${filename.replace(' ', '_')}.js`, 
-`const ${funcname} = () => {};
+let argIdx = 4;
+const args = [];
+while (process.argv[argIdx]) {
+  args.push(process.argv[argIdx]);
+  argIdx += 1;
+}
+
+write.sync(`2021/${filename.replace(' ', '_').toLowerCase()}.js`, 
+`const ${funcname} = (${process.argv[4] || ''}${process.argv[5] ? ', ' + process.argv[5] : ''}${process.argv[6] ? ', ' + process.argv[6] : ''}${process.argv[7] ? ', ' + process.argv[7] : ''}) => {};
 
 const tests = [
   {
